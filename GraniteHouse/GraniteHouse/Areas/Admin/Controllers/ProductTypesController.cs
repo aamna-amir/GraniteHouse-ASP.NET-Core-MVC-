e49f4fac3a5e4537a2452f7d97086a7d5c9b3228
Amna Amir <aamna48@outlook.com>
2020-09-07 15:53:13 +0500

    add view for Create

--- GraniteHouse/GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
+++ GraniteHouse/GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GraniteHouse.Data;
+using GraniteHouse.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GraniteHouse.Areas.Admin.Controllers
@@ -19,6 +20,26 @@ namespace GraniteHouse.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View(_db.ProductTypes.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Add(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productTypes);
         }
     }
 }
