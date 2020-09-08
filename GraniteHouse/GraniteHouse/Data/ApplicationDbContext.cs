ce1b5aee92bdeddc40c9662f449058f368ca1854
Amna Amir <aamna48@outlook.com>
2020-09-08 14:56:23 +0500

    add new model and controller SpecialTags and its view

--- GraniteHouse/GraniteHouse/Data/ApplicationDbContext.cs
+++ GraniteHouse/GraniteHouse/Data/ApplicationDbContext.cs
@@ -9,11 +9,13 @@ namespace GraniteHouse.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
 
         public DbSet<ProductTypes> ProductTypes { get; set; }
+        public DbSet<SpecialTags> SpecialTags { get; set; }
     }
 }
