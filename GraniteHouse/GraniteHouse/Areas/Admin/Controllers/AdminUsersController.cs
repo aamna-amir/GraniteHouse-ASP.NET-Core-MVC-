ce624699ade678a83fdd0b5054f81ca132df8d3e
Amna Amir <aamna48@outlook.com>
2020-09-14 16:56:56 +0500

    add Edit - Get Post action method for AdminUsers

--- GraniteHouse/GraniteHouse/Areas/Admin/Controllers/AdminUsersController.cs
+++ GraniteHouse/GraniteHouse/Areas/Admin/Controllers/AdminUsersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GraniteHouse.Data;
+using GraniteHouse.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GraniteHouse.Areas.Admin.Controllers
@@ -18,6 +19,42 @@ namespace GraniteHouse.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View(_db.ApplicationUsers.ToList());
+        }
+
+        // Get : Edit
+        public async Task<IActionResult> Edit(string id)
+        {
+            if(id == null || id.Trim().Length==0)
+            {
+                return NotFound();
+            }
+            var userFromDb = await _db.ApplicationUsers.FindAsync();
+            if(userFromDb==null)
+            {
+                return NotFound();
+            }
+            return View(userFromDb);
+        }
+
+
+        // Post : Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, ApplicationUser applicationUser)
+        {
+            if(id != applicationUser.Id)
+            {
+                return NotFound();
+            }
+            if(ModelState.IsValid)
+            {
+                ApplicationUser userFromDb = _db.ApplicationUsers.Where(u => u.Id == id).FirstOrDefault();
+                userFromDb.Name = applicationUser.Name;
+                userFromDb.PhoneNumber = applicationUser.PhoneNumber;
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(applicationUser);
         }
     }
 }
