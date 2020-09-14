0e93b2f4eee5d9213a2aa255bd8e6be99fc40b91
Amna Amir <aamna48@outlook.com>
2020-09-14 23:46:45 +0500

    add Delete - Get Post action methods in AdminUsers

--- GraniteHouse/GraniteHouse/Areas/Admin/Controllers/AdminUsersController.cs
+++ GraniteHouse/GraniteHouse/Areas/Admin/Controllers/AdminUsersController.cs
@@ -21,15 +21,17 @@ namespace GraniteHouse.Areas.Admin.Controllers
             return View(_db.ApplicationUsers.ToList());
         }
 
+
+
         // Get : Edit
         public async Task<IActionResult> Edit(string id)
         {
-            if(id == null || id.Trim().Length==0)
+            if (id == null || id.Trim().Length == 0)
             {
                 return NotFound();
             }
-            var userFromDb = await _db.ApplicationUsers.FindAsync();
-            if(userFromDb==null)
+            var userFromDb = await _db.ApplicationUsers.FindAsync(id);
+            if (userFromDb == null)
             {
                 return NotFound();
             }
@@ -42,11 +44,11 @@ namespace GraniteHouse.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(string id, ApplicationUser applicationUser)
         {
-            if(id != applicationUser.Id)
+            if (id != applicationUser.Id)
             {
                 return NotFound();
             }
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 ApplicationUser userFromDb = _db.ApplicationUsers.Where(u => u.Id == id).FirstOrDefault();
                 userFromDb.Name = applicationUser.Name;
@@ -55,6 +57,32 @@ namespace GraniteHouse.Areas.Admin.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(applicationUser);
+        }
+
+        // Get : Delete
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || id.Trim().Length == 0)
+            {
+                return NotFound();
+            }
+            var userFromDb = await _db.ApplicationUsers.FindAsync(id);
+            if (userFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(userFromDb);
+        }
+
+        // Post : Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeletePost(string id)
+        {
+            ApplicationUser userFromDb = _db.ApplicationUsers.Where(u => u.Id == id).FirstOrDefault();
+            userFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
